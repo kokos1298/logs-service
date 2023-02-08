e268e0db6d0001940d13cdccdcb60737ccaa39b8
kamil <kamila1207@wp.pl>
2023-02-08 21:27:18 +0100

    initial

---
+++ src/main/java/pl/niepracuj/logsservice/controller/LogsController.java
@@ -0,0 +1,24 @@
+package pl.niepracuj.logsservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logs")
+@RequiredArgsConstructor
+public class LogsController {
+
+    private final LogService logService;
+    private final LogMapper logMapper;
+
+    @PostMapping
+    public LogDto createLog(@RequestBody LogDto logDto) {
+        return logMapper.toDto(logService.saveLog(logMapper.toNewEntity(logDto)));
+
+    }
+
+}
+
